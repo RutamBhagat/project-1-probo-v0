3d67ce79afc7a528fe104fa8db1815c3f44a5517
RutamBhagat <rutambhagat@gmail.com>
2024-10-19 09:13:41 -0700

    add e2e tests

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 import express from 'express'
+import baseRouter from './routes/routes'
 
 export const prisma = new PrismaClient()
 export const app = express()
 
 app.use(express.json())
 
-app.get(`/user`, async (_req, res) => {
-  const result = await prisma.user.findMany()
-  res.json(result)
-})
-
-app.post(`/user`, async (req, res) => {
-  const { name, email } = req.body
-  try {
-    const result = await prisma.user.create({
-      data: {
-        name,
-        email,
-      },
-    })
-    res.json(result)
-  } catch (e) {
-    res.status(409).json({
-      error: 'User already exists!',
-    })
-  }
-})
+app.use('/api', baseRouter)
---
+++ src/routes/user-routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+import { prisma } from '../app'
+
+const router = Router()
+
+router.get(`/user`, async (_req, res) => {
+  const result = await prisma.user.findMany()
+  res.json(result)
+})
+
+router.post(`/user`, async (req, res) => {
+  const { name, email } = req.body
+  try {
+    const result = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    })
+    res.json(result)
+  } catch (e) {
+    res.status(409).json({
+      error: 'User already exists!',
+    })
+  }
+})
+
+export default router
